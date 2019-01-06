4bb913e4a8570e3df0e00af5ffa2061b8e9b1b15
Nisan <tnisan@gmail.com>
2019-01-06 18:43:25 +0200

    .

--- ex4/Server_Module.h
+++ ex4/Server_Module.h
@@ -19,18 +19,36 @@ Description		-
 	#define MAX_LOOPS 3
 	#define SEND_STR_SIZE 35
 	#define SERVER_ADDRESS_STR "127.0.0.1"
-	#define SERVER_PORT 2345
 	#define STRINGS_ARE_EQUAL( Str1, Str2 ) ( strcmp( (Str1), (Str2) ) == 0 )
 	#define MAX_MSG_SIZE 100
+
+	// Message Protocol
+	#define NEW_USER_REQUEST 1
+	#define NEW_USER_ACCEPTED 2
+	#define NEW_USER_DECLINED 3
+	#define GAME_STARTED 4
+	#define BOARD_VIEW 5
+	#define TURN_SWITCH 6
+	#define PLAY_REQUEST 7
+	#define PLAY_ACCEPTED 8
+	#define PLAY_DECLINED 9
+	#define GAME_ENDED 10
+	#define SEND_MESSAGE 11
+	#define RECEIVE_MESSAGE 12
+
+
 	// Global Variables ------------------------------------------------------------
 
 	HANDLE ThreadHandles[NUM_OF_WORKER_THREADS];
 	SOCKET ThreadInputs[NUM_OF_WORKER_THREADS];
+	unsigned short ServerPort;
 
 	// Function Declarations -------------------------------------------------------
 	void MainServer(char *argv[]);
 	static int FindFirstUnusedThreadSlot();
 	static void CleanupWorkerThreads();
 	static DWORD ServiceThread(SOCKET *t_socket);
+	int parseMessage(char *in_str, char *out_str);
+
 
 #endif // SOCKET_EXAMPLE_SERVER_H
